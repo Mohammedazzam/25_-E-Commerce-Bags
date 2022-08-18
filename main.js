257fe48b179080bcfdc63186060d6e22a3e9dbe8
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2022-08-18 11:18:26 +0300

    end all

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+// Header Animation
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('active', window.scrollY > 0);
+});
